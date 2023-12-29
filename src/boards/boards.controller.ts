81fc64dc42f0a9a8e00c8a83015b67d92898e04a
yusuShin <tlsdbtn0507@naver.com>
2023-12-29 17:23:42 +0900

    nest implement get, post

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,9 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './board.model';
 
 @Controller('boards')
 export class BoardsController {
-  constructor(private boardsService : BoardsController){
-    
+  constructor(private boardsService : BoardsService){}
+  
+  @Get('/')
+  getAllBoards():Board[]{
+    return this.boardsService.getAllBoards();
+  }
+  
+  @Post()
+  createBoard
+    ( @Body('title') title: string, @Body('description') description: string): Board {
+    return this.boardsService.createBoard(title,description)
   }
 }

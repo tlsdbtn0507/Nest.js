114c3b2bdcef4c50ca07146f64fb39291ccb0083
yusuShin <tlsdbtn0507@naver.com>
2024-01-02 21:02:27 +0900

    typeorm 설치와 디비 연결 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardsModule } from './boards/boards.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORM } from './configs/typeorm.config';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [
+    BoardsModule,
+    TypeOrmModule.forRoot(typeORM)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
